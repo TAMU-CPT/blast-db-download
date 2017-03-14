feb8e06875eaf5210396592f2045818fb1018bd1
Eric Rasche <rasche.eric@gmail.com>
2017-03-14 22:51:32 +0000

    asdf

--- canonical_phages.sh
+++ canonical_phages.sh
@@ -1,3 +1,3 @@
 gdrive export -f --mime text/tab-separated-values 11fFcc71nC0lKkifvI75FCAHNG1O83hPUiQTFqV14Dcs
-cat 'Canonical Phage Database.tsv' | awk -F'\t' '(NR>1){print $2}' > canonical_phages.list;
+cat 'Canonical Phage Database.tsv' | awk -F'\t' '(NR>1){print $2"\t"$1}' > canonical_phages.list;
 rm -f 'Canonical Phage Database.tsv'
